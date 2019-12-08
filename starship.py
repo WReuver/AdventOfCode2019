8066801c710af89f826dff7429445e9a2f717dea
Wesley Reuver <reuverwesley@gmail.com>
2019-12-08 12:30:47 +0100

    Rename file

---
+++ starship.py
@@ -0,0 +1,112 @@
+from enum import Enum
+import os
+
+class Ops():
+    @staticmethod
+    def add(a, b):
+        return int(a) + int(b)
+
+    @staticmethod
+    def multiply(a, b):
+        return int(a) * int(b)
+
+    @staticmethod
+    def divide(a, b):
+        return int(a)/int(b)
+
+    @staticmethod
+    def stop(a, b):
+        return
+
+class Starship:
+    opcodes = {
+        "1": Ops.add,
+        "2": Ops.multiply,
+        "3": Ops.divide,
+        "99": Ops.stop
+    }
+
+    memory = []
+
+    def __init__(self):
+        self.sum = 0
+
+    # Fuel calculation
+    def inputModules(self, input):
+        contents = open(input)
+        for line in contents.readlines():
+            self.fuelCounterUpper(int(line))
+
+    def fuelCounterUpper(self, mass):
+        fuel = int(mass / 3) - 2
+        self.sum += fuel
+        self.fuelFuelCounterUpper(fuel)
+
+    def fuelFuelCounterUpper(self, fuelMass):
+        fuelFuel = int(fuelMass/3) - 2
+        if fuelFuel > 0: 
+            self.sum += fuelFuel
+            self.fuelFuelCounterUpper(fuelFuel)
+        else: 
+            return
+
+    # Intcode calculations
+    def intcodeInput(self, intcodeFile):
+        intcodes = open(intcodeFile).readline()
+        intcodes = intcodes.strip('\n').split(',')
+
+        # Set the 1202 program alarm state requirements
+        intcodes[1] = '12'
+        intcodes[2] = '2'
+
+        self.intCodeCalculator(intcodes)
+        print(intcodes[0])
+
+    def intCodeCalculator(self, intcode):
+        i = 0
+        while i < len(intcode)-4:
+            # Determine the opcode and the a & b values
+            opcode = intcode[i]
+            a = intcode[int(intcode[i+1])]
+            b = intcode[int(intcode[i+2])]
+
+            # Determine the result of the operation
+            result = self.opcodes[str(opcode)](a, b)
+            # Ensure 99 halts the program
+            if result is None:
+                break
+
+            # Assign the result to the target position and increment the loop counter
+            targetPos = int(intcode[i+3])
+            intcode[targetPos] = result
+            i += 4
+
+santaShip = Starship()
+
+# Day 1 
+santaShip.inputModules('input.txt')
+print(santaShip.sum)
+
+# Day 2
+# Test input
+testInput1 = [1, 0, 0, 0, 99]
+testInput2 = [2, 3, 0, 3, 99]
+testInput3 = [2, 4, 4, 5, 99, 0]
+testInput4 = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+
+santaShip.intCodeCalculator(testInput1)
+print(testInput1[0])
+
+santaShip.intCodeCalculator(testInput2)
+print(testInput2[3])
+
+santaShip.intCodeCalculator(testInput3)
+print(testInput3[5])
+
+santaShip.intCodeCalculator(testInput4)
+print(testInput4[0])
+print(testInput4[4])
+
+# Actual input
+santaShip.intcodeInput('opcodeinput.txt')
+
